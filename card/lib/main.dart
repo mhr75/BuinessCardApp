e1d223583c0296cec9baab84cfe6428e9ef1beb5
Mahmudul Haque <i.m.mahmudul.haque@gmail.com>
2020-10-21 10:55:15 +0600

    commit 1
    With the commint 1, i have finished this small project.

--- card/lib/main.dart
+++ card/lib/main.dart
@@ -8,25 +8,112 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-      backgroundColor: Colors.redAccent[100],
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            CircleAvatar(
-              radius: 75.0,
-              backgroundImage: AssetImage('images/mahmudul.jpg'),
-            ),
-            Text(
-              "Mahmudul Haque",
-              style: TextStyle(
-                  fontSize: 25,
+          backgroundColor: Colors.lime,
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 75.0,
+                  backgroundImage: AssetImage('images/mahmudul.jpg'),
+                ),
+                Text(
+                  "Mahmudul Haque Rasel",
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "Flutter Developer",
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20,
+                    color: Colors.lime.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  width: 150,
+                  height: 20,
+                  child: Divider(
+                    color: Colors.lime.shade100,
+                  ),
+                ),
+                Card(
                   color: Colors.white,
-                  fontWeight: FontWeight.bold),
-            )
-          ],
-        ),
-      ),
-    ));
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 10,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.smartphone,
+                      color: Colors.lime.shade700,
+                    ),
+                    title: Text(
+                      "+08801880822541",
+                      style: TextStyle(
+                        color: Colors.lime.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                //SizedBox(
+                //  height: 10,
+                //),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 10,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.mail,
+                      color: Colors.lime.shade700,
+                    ),
+                    title: Text(
+                      "mahmudul.haque@g.bracu.ac.bd",
+                      style: TextStyle(
+                        color: Colors.lime.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 10,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.school,
+                      color: Colors.lime.shade700,
+                    ),
+                    title: Text(
+                      "BRAC University",
+                      style: TextStyle(
+                        color: Colors.lime.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
